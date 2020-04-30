1ddb51ce84404cd2a57753bd8d4bd69e8cea4c2e
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-04-30 10:59:39 +0200

    style(corewar/src/usage.c) replace quote char

--- corewar/src/usage.c
+++ corewar/src/usage.c
@@ -11,11 +11,11 @@ const char *usage_msg[] = {
     " [-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n",
     "DESCRIPTION\n",
     "-dump nbr_cycle dumps the memory after the nbr_cycle execution (if the",
-    "round isn’t\nalready over) with the following format: 32 bytes/line\nin ",
+    "round isn't already over) with the following format: 32 bytes/line\nin ",
     "hexadecimal (A0BCDEFE1DD3...)\n",
-    "-n prog_number sets the next program’s number. By default, the first"
+    "-n prog_number sets the next program's number. By default, the first"
     "free number\nin the parameter order\n",
-    "-a load_address sets the next program’s loading address. When no address",
+    "-a load_address sets the next program's loading address. When no address",
     " is\nspecified, optimize the addresses so that the processes are as far",
     "\naway from each other as possible. The addresses are MEM_SIZE modulo.\n",
     NULL
