f52aac0031cacb4ed78c7ed3556f8355322b71bd
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-05-11 19:05:36 +0200

    fix(corewar/src/main.c) add missing variable declaration

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -14,6 +14,7 @@ int main(int ac, char **av)
 {
     unsigned char *memory = NULL;
     data_read_t file = {0};
+    corewar_t corewar = {0};
 
     if (ac == 2 && !my_strcmp(av[1], "-h"))
         return usage(av[0], EXIT_SUCCESS);
