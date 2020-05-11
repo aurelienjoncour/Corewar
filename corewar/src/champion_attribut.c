7aae46e5289fe2bbb4c45280a7a96b7bbdba9384
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-05-11 21:07:31 +0200

    - Fix attribut_number

--- corewar/src/champion_attribut.c
+++ corewar/src/champion_attribut.c
@@ -7,10 +7,27 @@
 
 #include "corewar.h"
 
+static bool is_dup(champions_t *champ, int val)
+{
+    for (int i = 0; i < MAX_NB_CHAMPIONS; i++)
+        if (champ[i].prog_number == val)
+            return true;
+    return false;
+}
+
+static int get_value(champions_t *champ)
+{
+    for (int i = 0; i < MAX_NB_CHAMPIONS; i++)
+        if (!is_dup(champ, i))
+            return i;
+    return -1;
+}
+
 void attribut_number(champions_t *champ)
 {
     for (size_t i = 0; i < MAX_NB_CHAMPIONS; i++)
-        champ[i].prog_number = i;
+        if (champ[i].prog_number == -1)
+            champ[i].prog_number = get_value(champ);
 }
 
 void attribut_memory(champions_t *champ)
