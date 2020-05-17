c1b3de17f20648bc0b47d56320cc5a84bb7856c6
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-05-17 10:40:23 +0200

    fix(corewar/src/sti.c) replace unsigned short by short

--- corewar/src/operation/sti.c
+++ corewar/src/operation/sti.c
@@ -7,6 +7,28 @@
 
 #include "corewar.h"
 
+static size_t get_address(unsigned int *parameters, corewar_t *corewar,
+champions_t *champion)
+{
+    size_t address = 0;
+    short index = 0;
+
+    for (size_t i = 2; i < 6; i += 2) {
+        if (parameters[i] == T_REG)
+            address += champion->program->reg[parameters[i + 1] - 1];
+        if (parameters[i] == T_DIR) {
+            index = parameters[i + 1];
+            address += index;
+        }
+        if (parameters[i] == T_IND) {
+            index = get_short_in_memory(corewar->memory,
+                        (PC + parameters[i + 1]) % MEM_SIZE);
+            address += index;
+        }
+    }
+    return address;
+}
+
 int sti(corewar_t *corewar, champions_t *champion)
 {
     unsigned int *parameters = get_parameters(corewar->memory, PC);
@@ -16,15 +38,7 @@ int sti(corewar_t *corewar, champions_t *champion)
         return EXIT_ERROR;
     if (check_parameters(parameters, 11) == false)
         return EXIT_FAILURE;
-    for (size_t i = 2; i < 6; i += 2) {
-        if (parameters[i] == T_REG)
-            address += champion->program->reg[parameters[i + 1] - 1];
-        if (parameters[i] == T_DIR)
-            address += parameters[i + 1];
-        if (parameters[i] == T_IND)
-            address += get_short_in_memory(corewar->memory,
-                        (PC + parameters[i + 1]) % MEM_SIZE);
-    }
+    address = get_address(parameters, corewar, champion);
     write_int_in_mem(corewar->memory, (PC + (address % IDX_MOD) % MEM_SIZE),
     champion->program->reg[parameters[1] - 1]);
     free(parameters);
--- corewar/src/operation/zjmp.c
+++ corewar/src/operation/zjmp.c
@@ -10,14 +10,16 @@
 int zjmp(corewar_t *corewar, champions_t *champion)
 {
     unsigned int *parameters = get_parameters(corewar->memory, PC);
+    short index = 0;
 
     if (parameters == NULL)
         return EXIT_ERROR;
     if (!check_parameters(parameters, 9))
         return EXIT_FAILURE;
+    index = parameters[1];
     if (champion->program->carry) {
-        champion->program->pc += parameters[1] % IDX_MOD;
-        champion->program->pc %= MEM_SIZE;
+        champion->program->pc = champion->program->pc + index % IDX_MOD;
+        champion->program->pc = champion->program->pc % MEM_SIZE;
     }
     free(parameters);
     return EXIT_SUCCESS;
